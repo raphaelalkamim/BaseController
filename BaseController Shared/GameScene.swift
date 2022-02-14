19fa8b5adbe8b447437e45dc81f1854a7bb16f0f
raphaelalkamim <83611337+raphaelalkamim@users.noreply.github.com>
2022-02-14 17:35:32 -0300

    Testes

--- BaseController Shared/GameScene.swift
+++ BaseController Shared/GameScene.swift
@@ -9,14 +9,28 @@
 import SpriteKit
 import GameController
 
-class GameScene: SKScene, JoystickDelegate {
-
+class GameScene: SKScene, JoystickDelegate, SKPhysicsContactDelegate {
+    
     var animal = SKSpriteNode(imageNamed: "parrot")
+    var monster = SKSpriteNode(imageNamed: "monster")
+    var monsterInitialPosition: CGPoint {
+        let x = self.size.width - 60
+        let y = self.size.height - 60
+        
+        return CGPoint(x: x, y: y)
+    }
+    
     let initialPosition = CGPoint(x: 100, y: 100)
     let multiplier: CGFloat = 10.0
     let joystickController: JoystickController = JoystickController()
     var lastActionTime: TimeInterval = TimeInterval.zero
     let whaitForNextAction: Double = 1
+    
+    var dx:CGFloat = 0
+    var dy:CGFloat = 0
+    
+    var monsterX: CGFloat = -1
+    var monsterY: CGFloat = 0
     
     class func newGameScene() -> GameScene {
         let scene = GameScene()
@@ -28,7 +42,29 @@ class GameScene: SKScene, JoystickDelegate {
         joystickController.delegate = self
         joystickController.observeForGameControllers()
         
+        self.physicsWorld.contactDelegate = self
+        self.physicsWorld.gravity = .zero
+        
+        
         createAnimal()
+                
+        monster.physicsBody = SKPhysicsBody(texture: monster.texture!, size: monster.texture!.size())
+        monster.physicsBody?.isDynamic = true
+        monster.physicsBody?.categoryBitMask = 1 << 2
+
+        insertMonster()
+
+
+       
+ 
+    }
+    
+    
+    func didBegin(_ contact: SKPhysicsContact) {
+        print(contact.bodyA)
+        print(contact.bodyB)
+        resetAnimal()
+        monster.position = monsterInitialPosition
     }
     
     func createAnimal() {
@@ -39,27 +75,60 @@ class GameScene: SKScene, JoystickDelegate {
         animal = SKSpriteNode(imageNamed: animalName[index])
         animal.position = initialPosition
         
+        animal.physicsBody = SKPhysicsBody(texture: animal.texture!, size: animal.texture!.size())
+        animal.physicsBody?.isDynamic = true
+        animal.physicsBody?.categoryBitMask = 1 << 1
+        animal.physicsBody?.contactTestBitMask = 1 << 2
+        
         self.addChild(animal)
+    }
+    
+    func insertMonster() {
+        monster.setScale(0.1)
+        monster.position = monsterInitialPosition
+        
+        self.addChild(monster)
     }
     
     func moveAnimal(dx: CGFloat, dy: CGFloat) {
         var xValue = animal.position.x + dx * multiplier
         var yValue = animal.position.y + dy * multiplier
         
-        if xValue > self.size.width {
-            xValue = 0
+        let animalWidth = animal.size.width/2
+        let animalHeight = animal.size.height/2
+        
+        //        if xValue > self.size.width {
+        //            xValue = 0
+        //        }
+        //        if xValue < 0 {
+        //            xValue = self.size.width
+        //        }
+        //        if yValue > self.size.height {
+        //            yValue = 0
+        //        }
+        //        if yValue < 0 {
+        //            yValue = self.size.height
+        //        }
+        
+        if xValue > self.size.width - animalWidth {
+            xValue = self.size.width - animalWidth
         }
-        if xValue < 0 {
-            xValue = self.size.width
+        if xValue < animalWidth {
+            xValue = animalWidth
         }
-        if yValue > self.size.height {
-            yValue = 0
+        if yValue > self.size.height - animalHeight {
+            yValue = self.size.height  - animalHeight
         }
-        if yValue < 0 {
-            yValue = self.size.height
+        if yValue < animalHeight {
+            yValue = animalHeight
         }
         
         animal.position = CGPoint(x: xValue, y: yValue)
+    }
+    
+    func moveMonster(xValue: CGFloat) {
+        monster.position.x = monster.position.x + xValue
+
     }
     
     func resetAnimal() {
@@ -69,21 +138,23 @@ class GameScene: SKScene, JoystickDelegate {
     
     override func update(_ currentTime: TimeInterval) {
         joystickController.update(currentTime)
+        moveAnimal(dx: dx, dy: dy)
+        moveMonster(xValue: monsterX)
     }
     
-    #if os(watchOS)
+#if os(watchOS)
     override func sceneDidLoad() {
         self.setUpScene()
     }
-    #else
+#else
     override func didMove(to view: SKView) {
         self.setUpScene()
     }
-    #endif
+#endif
     
     //MARK :- iOS and tvOS
 #if os(iOS) || os(tvOS)
-// Touch-based event handling
+    // Touch-based event handling
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {}
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {}
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {}
@@ -91,7 +162,7 @@ class GameScene: SKScene, JoystickDelegate {
 #endif
     
 #if os(OSX)
-// Mouse-based event handling
+    // Mouse-based event handling
     override func mouseDown(with event: NSEvent) {}
     override func mouseDragged(with event: NSEvent) {}
     override func mouseUp(with event: NSEvent) {}
@@ -120,8 +191,6 @@ class GameScene: SKScene, JoystickDelegate {
     
     func buttonPressed(command: GameCommand) {
         print("pressed: \(command)")
-        var dx:CGFloat = 0
-        var dy:CGFloat = 0
         
         switch command {
         case .UP:
@@ -137,16 +206,27 @@ class GameScene: SKScene, JoystickDelegate {
             return
         }
         
-        self.moveAnimal(dx: dx, dy: dy)
     }
     
     func buttonReleased(command: GameCommand) {
         print("released: \(command)")
+        switch command {
+        case .UP:
+            dy = 0
+        case .DOWN:
+            dy = 0
+        case .RIGHT:
+            dx = 0
+        case .LEFT:
+            dx = 0
+        default:
+            return
+        }
     }
     
     func joystickUpdate(_ currentTime: TimeInterval){
         if let gamePadLeft = joystickController.gamePadLeft {
-            if gamePadLeft.xAxis.value != 0 || gamePadLeft.xAxis.value != 0{
+            if gamePadLeft.xAxis.value != 0 || gamePadLeft.yAxis.value != 0{
                 let dx: CGFloat = CGFloat(gamePadLeft.xAxis.value)
                 let dy: CGFloat = CGFloat(gamePadLeft.yAxis.value)
                 // print("dpad: \(dx), \(dy)")
--- BaseController iOS/GameViewController.swift
+++ BaseController iOS/GameViewController.swift
@@ -24,6 +24,7 @@ class GameViewController: UIViewController {
         skView.ignoresSiblingOrder = true
         skView.showsFPS = true
         skView.showsNodeCount = true
+        skView.showsPhysics = true
     }
 
     override var shouldAutorotate: Bool {
